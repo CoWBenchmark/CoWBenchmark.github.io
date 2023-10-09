dc6be4cc32013412fd720ff21ddf2ff3ffadc122
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2023-10-09 12:34:23 +0000

    Deploying to gh-pages from @ CoWBenchmark/CoWBenchmark.github.io@474a8be8fdd53de1a5af1b91f28267d0f8ba3619 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,7 +12,6 @@ let toggleTheme = (theme) => {
 let setTheme = (theme) =>  {
   transTheme();
   setHighlight(theme);
-  setGiscusTheme(theme);
 
   if (theme) {
     document.documentElement.setAttribute("data-theme", theme);
@@ -40,23 +39,6 @@ let setHighlight = (theme) => {
     document.getElementById("highlight_theme_dark").media = "none";
     document.getElementById("highlight_theme_light").media = "";
   }
-}
-
-
-let setGiscusTheme = (theme) => {
-
-  function sendMessage(message) {
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (!iframe) return;
-    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
-  }
-
-  sendMessage({
-    setConfig: {
-      theme: theme
-    }
-  });
-
 }
 
 
